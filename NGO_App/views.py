fe8dc83744d62d79db4771b57dc7f896b68d8e0e
Your Name <you@example.com>
2021-04-06 18:50:11 +0530

    refresh

--- NGO_App/views.py
+++ NGO_App/views.py
@@ -15,7 +15,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status,generics,mixins
 from rest_framework.views import APIView
-from rest_framework.authentication import SessionAuthentication,BasicAuthentication,TokenAuthentication
+from rest_framework.authentication import BasicAuthentication,TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated,IsAdminUser,AllowAny
 from rest_framework.decorators import authentication_classes,permission_classes
